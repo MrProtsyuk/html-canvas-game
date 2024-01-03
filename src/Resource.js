c303b6271209399bf392e04a9e4a6c1823626e93
Mark Protsyuk <marcprotsyuk@gmail.com>
2024-01-03 12:47:40 -0800

    first commit

---
+++ src/Resource.js
@@ -0,0 +1,10 @@
+class Resources {
+  constructor() {
+    //Everything that is to be downloaded
+    this.toLoad = {
+      sky: "/sprites/sky.png",
+    };
+
+    //A space to keep all of the images
+  }
+}
